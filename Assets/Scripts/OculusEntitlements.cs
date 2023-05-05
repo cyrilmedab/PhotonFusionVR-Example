d8408ed2486f832b0a74321e69373980ed53d25b
cyrilmedab <cyrildmed@gmail.com>
2023-05-05 17:54:47 -0400

    Updated to URP

--- Assets/Scripts/OculusEntitlements.cs
+++ Assets/Scripts/OculusEntitlements.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Oculus.Platform;
 
 public class OculusEntitlements : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Awake()
     {
-        
+        try
+        {
+            Core.AsyncInitialize();
+            Entitlements.IsUserEntitledToApplication().OnComplete(EntitlementCallback);
+        }
+        catch (UnityException e)
+        {
+            Debug.LogError("Platform failed to initialize due to exception.");
+            Debug.LogException(e);
+            UnityEngine.Application.Quit();
+        }
     }
+    
+    // Called when the Meta Quest Platform completes the async entitlement check request and a result is available
+    private void EntitlementCallback(Message msg)
+    {
+        if (msg.IsError) // User failed entitlement check 
+        {
+            // Implements a default behavior for an entitlement check failure -- Log the failure and exit the app
+            Debug.LogError("You are NOT entitled to use this app.");
+            UnityEngine.Application.Quit();
+        }
+        else // User passed entitlement check
+        {
+            // Log the succeeded entitlement check for debugging
+            Debug.Log("You are entitled to use this app.");
+        }
+    }
+    
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
