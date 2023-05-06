a57dde4ae190d195a3d608c84b3e78aa1b9c8732
cyrilmedab <cyrildmed@gmail.com>
2023-05-06 18:26:14 -0400

    First example with Photon Pun 2.0 outlined. Publishing settings to be changed before test build

--- Assets/Scripts/OculusInitializer.cs
+++ Assets/Scripts/OculusInitializer.cs
@@ -3,16 +3,29 @@ using System.Collections;
 using Oculus.Avatar2;
 using UnityEngine;
 using Oculus.Platform;
-using UnityEngine.Serialization;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class OculusInitializer : MonoBehaviour
+
+public class OculusInitializer : MonoBehaviourPunCallbacks
 {
     public UInt64 userId = 0;
+    public bool isServer;
     
-    [SerializeField] private StreamingAvatar _streamingAvatar;
+    //[SerializeField] private StreamingAvatar _streamingAvatar;
     
     private string _sampleAvatar = "sampleAvatar";
     
+    [SerializeField] private int playerNum = 0;
+    [SerializeField] private int playersPerRoom = 20;
+    // Need to add code to make sure this is always ^^ size, either cutting it short or adding points
+    [SerializeField] private Transform[] playerSpawnPoints;
+    // won't need this after validating playerSpawnPoints array
+    private int _spawnInd = 0;
+
+    public GameObject player, playerPrefab;
+    public Transform StartRig;
+
     private void Awake()
     {
         try
@@ -41,11 +54,11 @@ public class OculusInitializer : MonoBehaviour
         {
             // Log the succeeded entitlement check for debugging
             Debug.Log("You are entitled to use this app.");
-            StartCoroutine(StartOVRPlatform());
+            StartCoroutine(StartOvrPlatform());
         }
     }
 
-    private IEnumerator StartOVRPlatform()
+    private IEnumerator StartOvrPlatform()
     {
         // Ensure OvrPlatform is Initialized
         if (OvrPlatformInit.status == OvrPlatformInitStatus.NotStarted)
@@ -71,21 +84,69 @@ public class OculusInitializer : MonoBehaviour
             // Get User ID
             Users.GetLoggedInUser().OnComplete(message =>
             {
-                if (!message.IsError)
+                if (message.IsError)
                 {
-                    userId = message.Data.ID;
-                    // TODO: build multiplayer login room
-                    _streamingAvatar.gameObject.SetActive(true);
-                    _streamingAvatar.StartAvatar(this);
+                    var e = message.GetError();
+                    OvrAvatarLog.LogError($"Error loading CDN avatar: {e.Message}. " +
+                                          "Falling back to local avatar", _sampleAvatar);
                 }
                 else
                 {
-                    var e = message.GetError();
-                    OvrAvatarLog.LogError($"Error loading CDN avatar: {e.Message}. Falling back to local avatar", _sampleAvatar);
+                    userId = message.Data.ID;
+                    ConnectToServer();
+                    // TODO: build multiplayer login room
+                    //_streamingAvatar.gameObject.SetActive(true);
+                    //_streamingAvatar.StartAvatar(this);
                 }
             });
         }
     }
 
-    
+    public void ConnectToServer()
+    {
+        PhotonNetwork.SendRate = 30;
+        PhotonNetwork.SerializationRate = 20;
+        PhotonNetwork.AutomaticallySyncScene = true;
+        
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions
+        {
+            MaxPlayers = (byte)playersPerRoom,
+            IsVisible = true,
+            IsOpen = true
+        };
+
+        PhotonNetwork.JoinOrCreateRoom("StartRoom", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        isServer = true;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        // This is where you can do an if-statement to change spawn positions as new people join
+        int spawnPosCount = playerSpawnPoints.Length;
+        StartRig = playerSpawnPoints[_spawnInd]; //add a .transform here?
+        
+        playerNum++;
+        _spawnInd = (_spawnInd + 1) % spawnPosCount;
+
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        object[] userId0 = { Convert.ToInt64(userId) };
+        player = PhotonNetwork.Instantiate(playerPrefab.name, StartRig.position, StartRig.rotation, 0, userId0);
+        StartRig.gameObject.SetActive(false);
+    }
 }
